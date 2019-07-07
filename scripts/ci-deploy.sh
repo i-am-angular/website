012fa16e4881ece6f9724537131a3a9e2cc98461
unknown <nasreddine.skandrani@gmail.com>
2019-07-07 13:22:14 -0400

    update ingress
    2

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -10,14 +10,22 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/deployment.yml >./kube/deployment.yml.out
-mv ./kube/deployment.yml.out ./kube/.yml
+envsubst <./kube/app/deployment.yml >./kube/app/deployment.yml.out
+mv ./kube/app/deployment.yml.out ./kube/app/.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
 
 ./kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
+  apply -f ./kube/app
+
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./kube/ingress
