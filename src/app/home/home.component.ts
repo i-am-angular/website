b5ee38faecb0a0a33165f01f67aa2e56b28543bd
unknown <nasreddine.skandrani@gmail.com>
2019-07-21 20:12:49 -0400

    add new links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,9 +28,9 @@ export class HomeComponent {
       {
         images: { // Vue
           github: {
-            label: 'Contribute On Github',
+            label: 'On Github',
             src: 'https://miro.medium.com/max/1838/1*8_fSyyWCZg9quYkqLKSlsg.png',
-            href: ''
+            href: 'https://github.com/vuejs/vue'
           },
           formation: {
             label: 'Formations',
@@ -38,77 +38,77 @@ export class HomeComponent {
             href: ''
           },
           conf: {
-            label: 'Conferences calendar',
+            label: 'Conferences',
             src: 'https://s3.amazonaws.com/revue/items/images/002/086/577/mail/19390746_10154637520598091_8432004182598006886_o.jpg?1498727193',
-            href: ''
+            href: 'https://www.vuemastery.com/conferences/vueconf-us-2019'
           },
           libs: {
             label: 'Libraries used with',
             src: 'https://user-images.githubusercontent.com/7110136/29002857-9e802f08-7ab4-11e7-9c31-604b5d0d0c19.png',
-            href: ''
+            href: 'https://github.com/vuejs/awesome-vue'
           },
           job: {
             label: 'Jobs',
             src: 'https://cdn-images-1.medium.com/max/280/1*oYqTFcLMerYbhTJqyQ8AQg@2x.jpeg',
-            href: ''
+            href: 'https://vuejobs.com/'
           },
         }
       },
       {
         images: { // Angular
           github: {
-            label: 'Contribute On Github',
+            label: 'On Github',
             src: 'https://scotch-res.cloudinary.com/image/upload/dpr_3,w_350,q_auto:good,f_auto/media/1/43RVTN2dQKGJxpMDUwUu_jClEMwH.png',
-            href: ''
+            href: 'https://github.com/angular/angular'
           },
           formation: {
             label: 'Formations',
-            src: 'https://www.cabaretelanguage.com/wp-content/uploads/2016/01/books.jpg',
+            src: 'https://img.huffingtonpost.com/asset/5cd52750200000320096a5a0.jpeg?ops=scalefit_630_noupscale',
             href: ''
           },
           conf: {
-            label: 'Conferences calendar',
+            label: 'Conferences',
             src: 'https://www.ng-conf.org/wp-content/uploads/2018/11/widdisonphotography_0483-900x720.jpg',
-            href: ''
+            href: 'https://www.youtube.com/user/ngconfvideos/videos'
           },
           libs: {
             label: 'Libraries used with',
             src: 'https://ngrx.io/assets/images/badge.svg',
-            href: ''
+            href: 'https://github.com/PatrickJS/awesome-angular'
           },
           job: {
             label: 'Jobs',
-            src: 'https://media.licdn.com/dms/image/C4D1BAQFz6YdFumIriA/company-background_10000/0?e=2159024400&v=beta&t=qyDEvXpo6SgPl6Q5TrhaCAo2m1l_Rhv_eD-1e71CEEM',
-            href: ''
+            src: 'https://www.42jobs.io/assets/angular/default_company_logo-9ba3b5df8d62e95a7c254ee4d7a05f7cff878277016c40d1e893b96e7df64823.png',
+            href: 'https://www.42jobs.io/angular'
           },
         }
       },
       {
         images: { // React
           github: {
-            label: 'Contribute On Github',
+            label: 'On Github',
             src: 'https://react.rocks/images/converted/react-faq.jpg',
-            href: ''
+            href: 'https://github.com/facebook/react'
           },
           formation: {
             label: 'Formations',
-            src: 'https://content.linkedin.com/content/dam/business/marketing-solutions/global/en_US/blog/20b810.jpg',
+            src: 'https://img.huffingtonpost.com/asset/5cd52750200000320096a5a0.jpeg?ops=scalefit_630_noupscale',
             href: ''
           },
           conf: {
-            label: 'Conferences calendar',
+            label: 'Conferences',
             src: 'https://pbs.twimg.com/media/DqYZbs9WkAELgOp.jpg',
-            href: ''
+            href: 'https://www.youtube.com/channel/UCz5vTaEhvh7dOHEyd1efcaQ/videos'
           },
           libs: {
             label: 'Libraries used with',
             src: 'https://redux.js.org/img/redux-logo-twitter.png',
-            href: ''
+            href: 'https://github.com/enaqx/awesome-react'
           },
           job: {
             label: 'Jobs',
-            src: 'https://reactjobsboard.com/reactjobsboard.jpg',
-            href: ''
+            src: 'https://pbs.twimg.com/profile_images/955038447005249536/mgkD5oqr.jpg',
+            href: 'https://www.42jobs.io/react'
           },
         }
       }
